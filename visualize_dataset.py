7d18934f07804cbe565e944a9752bd56ed890d77
buobm <mischa.buob@gmail.com>
2023-05-16 18:36:59 +0200

    add visualize_dataset

---
+++ visualize_dataset.py
@@ -0,0 +1,44 @@
+import os
+import matplotlib.pyplot as plt
+import cv2
+import numpy as np
+
+def normalize_image(image):
+    # Normalize image to uint8
+    image = (image - np.min(image)) / (np.max(image) - np.min(image))
+    image = (image * 255).astype(np.uint8)
+    return image
+
+def plot_images(image1, image2):
+    fig, axs = plt.subplots(1, 2)
+
+    axs[0].imshow(image1)
+    axs[0].set_title('Image 1')
+
+    axs[1].imshow(image2)
+    axs[1].set_title('Image 2')
+
+    plt.show()
+
+def process_images(folder1, folder2):
+    for filename in os.listdir(folder1):
+        if filename.endswith('.png') and filename.startswith('02002'):
+            image_path1 = os.path.join(folder1, filename)
+            image_path2 = os.path.join(folder2, filename)
+
+            if os.path.isfile(image_path2):
+                image1 = cv2.imread(image_path1, cv2.IMREAD_UNCHANGED)
+                image2 = cv2.imread(image_path2, cv2.IMREAD_UNCHANGED)
+
+                # Normalize images
+                image1 = normalize_image(image1)
+                image2 = normalize_image(image2)
+
+                # Plot images
+                plot_images(image1, image2)
+
+# Specify the folders containing the images
+folder1 = 'dataset/samMasks/inputs'
+folder2 = 'dataset/samMasks/masks'
+
+process_images(folder1, folder2)
